4dead5dd8631d2cd8313b235e3c8b68d56fb955a
Prajwal Zalaki <prajwalzalaki@192.168.0.103>
2025-03-06 12:35:39 +0530

    challenge-3

--- challenge-3/main.tf
+++ challenge-3/main.tf
@@ -2,8 +2,14 @@
 variable "instance_config" {
   type = map
   default = {
-    instance1 = { instance_type = "t2.micro", ami = "ami-03a6eaae9938c858c" }
-    instance2 = { instance_type = "t2.small", ami = "ami-053b0d53c279acc90" }
+    instance1 = { instance_type = "t2.micro", ami = "ami-05b10e08d247fb927" }
+    instance2 = { instance_type = "t2.small", ami = "ami-04b4f1a9cf54c11d0" }
   }
 }
 
+resource "aws_instance" "web" {
+  for_each = var.instance_config
+
+  ami           = each.value.ami
+  instance_type = each.value.instance_type
+}
